709cf718862c2a578aaf1ace726892dd2b856885
Kenny Johnson <kenny.amorim@loglabdigital.com.br>
2024-10-18 09:30:00 -0300

    chore: configurar estrutura do monorepo
    - Adicionar package.json raiz com scripts Turbo
    - Configurar pnpm workspaces
    - Adicionar turbo.json para pipeline de build
    - Configurar TypeScript base compartilhado
    - Adicionar ESLint base

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  rules: {
+    "no-unused-vars": "off",
+  },
+};
+
